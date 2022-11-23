6ca60df1445c56a9fb39b55b6786707b0c22f9ea
Inessa <modina.inessa@gmail.com>
2022-11-23 15:47:12 -0800

    Components templets

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,46 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../logo.svg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 
 export default class NavBar extends Component {
   render() {
     return (
-      <nav className='navbar navbar-expand-sm bg-primary navbar-dark px-sm-5'>
-        {/* {https://www.iconfinder.com/icons/1243689/call_phone_icon
-        Creative Commons (Attribution 3.0 Unported);
-        https://www.iconfinder.com/Makoto_msk} */}
-        <Link to='/'>
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+        <Link to="/">
           <img src={logo} alt="store logo"
-          className='navbar-brand'/>
+          className="navbar-brand"/>
         </Link>
         <ul className="navbar-nav align-items-center">
-          <li className='nav-item ml-5'>
-            <Link to='/' className='nav-link text-capitalize'>
+          <li className="nav-item ms-5">
+            <Link to="/" className="nav-link">
               products
             </Link>
           </li>
         </ul>
-        <Link to='/cart' className='ms-auto'>
-          <button className='text-capitalize'>
-          <FontAwesomeIcon icon={faCartPlus} />
+        <Link to="/cart" className="ms-auto">
+          <ButtonContainer>
+            <span className="me-2">
+              <FontAwesomeIcon icon={faCartPlus} />
+            </span>
           my cart
-          </button>
+          </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     )
   }
 }
+
+const NavWrapper =  styled.nav`
+background: var(--mainBlue);
+.nav-link{
+  color: var(--mainWhite)!important;
+  font-size: 1.3rem;
+  text-transform: capitalize;
+}
+`
+
+
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
-import Product from './Product';
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+import{ storeProducts } from "../data.js";
 
 export default class ProductList extends Component {
+  state={
+    products: storeProducts,
+  }
   render() {
+    console.log(this.state.products)
     return (
-      <div>
-        <h3>Hello from product list</h3>
-        <Product />
+      <>
+      <div className="py-5">
+        <Title name="our" title="products"/>
       </div>
+      <div className="container">
+        <div className="row">
+
+        </div>
+      </div>
+      </>
     )
   }
 }
