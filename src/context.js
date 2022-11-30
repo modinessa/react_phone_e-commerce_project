ff41f50f60155500171e0a3edf702844d35fc977
Inessa <modina.inessa@gmail.com>
2022-11-30 13:29:26 -0800

    Add Cart component UI and adding to cart logic

--- src/context.js
+++ src/context.js
@@ -9,9 +9,12 @@ class ProductProvider extends Component {
     state = {
         products: [],
         detailProduct: detailProduct,
-        cart:[],
+        cart: [],
         modalOpen: false,
         modalProduct: detailProduct,
+        cartSubTotal:0,
+        cartTax:0,
+        cartTotal:0,
     }
 
     componentDidMount() {
@@ -73,6 +76,22 @@ class ProductProvider extends Component {
         })
     }
 
+    increment = (id) => {
+        console.log('this is increment method');
+    }
+
+    decrement = (id) => {
+        console.log('this is decrement method');
+    }
+
+    removeItem = (id) => {
+        console.log('item removed');
+    }
+
+    clearCart = () => {
+        console.log('cart was cleared');
+    }
+
     render() {
         return (
         <ProductContext.Provider value={{
@@ -80,7 +99,11 @@ class ProductProvider extends Component {
             handleDetail: this.handleDetail,
             addToCart: this.addToCart,
             openModal:this.openModal,
-            closeModal: this.closeModal
+            closeModal: this.closeModal,
+            increment:this.increment,
+            decrement:this.decrement,
+            removeItem:this.removeItem,
+            clearCart:this.clearCart,
         }}>
             {this.props.children}
         </ProductContext.Provider>
