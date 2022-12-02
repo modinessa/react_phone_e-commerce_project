20fbebbd9fc06ef6ea8a0ddf05f050c003e9bfdb
Inessa <modina.inessa@gmail.com>
2022-12-01 16:13:38 -0800

    Add Delete Item and clear cart functionaliti

--- src/context.js
+++ src/context.js
@@ -56,6 +56,9 @@ class ProductProvider extends Component {
                 products: tempProducts,
                 cart:[...this.state.cart, product]
             }
+        },
+        () => {
+            this.addTotals();
         })
         
     }
@@ -85,11 +88,49 @@ class ProductProvider extends Component {
     }
 
     removeItem = (id) => {
-        console.log('item removed');
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+        tempCart = tempCart.filter(item => item.id != id);
+
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.count = 0;
+
+        this.setState(() => {
+            return {
+                cart: [...tempCart],
+                products: [...tempProducts]
+            }
+        }, () => {
+            this.addTotals();
+        })
     }
 
     clearCart = () => {
-        console.log('cart was cleared');
+        this.setState(() => {
+            return {
+                cart: []
+            }
+        }, () => {
+            this.setProducts();
+        })
+    }
+
+    addTotals = () => {
+        let subTotal = 0;
+        this.state.cart.map(item => (subTotal += item.total));
+        const tempTax = subTotal * 0.1;
+        const tax = parseFloat(tempTax.toFixed(2));
+        const  total = subTotal + tax;
+        this.setState(() => {
+            return {
+                cartSubTotal: subTotal,
+                cartTax: tax,
+                cartTotal:total
+            }
+        })
     }
 
     render() {
